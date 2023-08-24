0bfc4d20cbaef17d9f4aafb360211ef044784f17
omodara-prog <oiretunde@gmail.com>
2023-08-24 12:20:39 +0100

    add workflow

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+  /*build: {
     outDir: "dist",
-  },
-  base: "/kodecamp-link-tree/",
+  },*/
+  base: "/kodecamp-link-tree/"
 });
